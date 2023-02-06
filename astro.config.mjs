a4da6b0893ed8dd78be6257e3e1bcc149f45c20b
dai <dosada@gmail.com>
2023-02-06 14:12:19 +0900

    added n
    ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-export default {
-  renderers: [
-    '@astrojs/renderer-preact'
-  ]
-};
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  renderers: ['@astrojs/renderer-preact'],
+  output: "server",
+  adapter: netlify()
+});
